8cc3969aa2d6e134146d49999ff0caa43c5f751b
Ignacio <imoon521@gmail.com>
2018-11-13 23:51:06 -0600

    direcciones

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,11 @@ export class AuthService {
   constructor(private http:HttpService) { }
 
   public isLogged(){
+    if(environment.production === true){
+      if(!location.origin.includes('https://www.calculaminota.com')){
+        location.href = 'https://www.calculaminota.com' + location.pathname;
+      }
+    }
     return this.http.get('usuarios/isLogged');
   }
 
